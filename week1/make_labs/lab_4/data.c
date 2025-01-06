486966db5353666bb919b1059940221e56bd0baa
Seb-sti1 <65665540+seb-sti1@users.noreply.github.com>
2025-01-06 16:31:39 +0100

    make ex4

--- week1/make_labs/lab_4/data.c
+++ week1/make_labs/lab_4/data.c
@@ -1,17 +1,18 @@
 #include <stdio.h>
 #include "data.h"
+#include <math.h>
 
-void 
-data(void) { 
-    double x, y;
-    int i;
+void
+data(void) {
+  double x, y;
+  int i;
 
-    printf("Working on data...\n");
+  printf("Working on data...\n");
 
-    for(i = 0; i < 20; i++ ) {
-	x = 0.5 * M_PI * i;
-	y = sin(x);
-	printf("%lf\t%+lf\n", x, y);
-    }
-    printf("...done.\n");
+  for (i = 0; i < 20; i++) {
+    x = 0.5 * M_PI * i;
+    y = sin(x);
+    printf("%lf\t%+lf\n", x, y);
+  }
+  printf("...done.\n");
 };
