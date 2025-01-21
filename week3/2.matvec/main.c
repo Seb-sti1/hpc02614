0c010306cf17b8fe2897216aec0ae6bbe95b64ba
Seb-sti1 <65665540+seb-sti1@users.noreply.github.com>
2025-01-21 14:37:59 +0100

    ex w3d2

--- week3/2.matvec/main.c
+++ week3/2.matvec/main.c
@@ -59,7 +59,7 @@ int main() {
 
   printf("\n%d\n", n);
   double t1 = omp_get_wtime();
-#pragma omp target teams num_teams(N/2) thread_limit(1) distribute parallel for \
+#pragma omp target teams num_teams(n/2) thread_limit(1) distribute parallel for \
 map(to:n, m[0:n/2*n], v[0:n]) map(tofrom:c[0:n/2]) device(0)
   for (int i = 0; i < n / 2; i++) {
     double sum = 0;
@@ -69,7 +69,7 @@ map(to:n, m[0:n/2*n], v[0:n]) map(tofrom:c[0:n/2]) device(0)
     c[i] = sum;
   }
 
-#pragma omp target teams num_teams(N/2) thread_limit(1) distribute parallel for \
+#pragma omp target teams num_teams(n/2) thread_limit(1) distribute parallel for \
 map(to:n, m[n/2*n:n/2*n], v[0:n]) map(tofrom:c[n/2:n/2]) device(1)
   for (int i = n / 2; i < n; i++) {
     double sum = 0;
